d66a1f03d9a4df71672a00472327fb921e69b815
Gugarauj07 <gugaraujo01@gmail.com>
2022-06-15 18:02:21 -0400

    15/06/2022

---
+++ Codes/Indutivo/Indutivo.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h> 
+#define pinVEL 2 // Pino de interrupção para rotação da roda
+
+unsigned long Velocidade_millisInicial = 0; //tempo inicial para velocidade  
+volatile byte pulsosVEL = 0; //contador de pulsos para velocidade
+float VEL = 0; //velocidade em km/h
+float RAIO_RODA = 0.266;
+int contadorVEL = 0;
+
+void velocidade()
+{
+  if((millis() - Velocidade_millisInicial) > 1000){
+    
+    noInterrupts();
+    VEL = (pulsosVEL*0.04625*2*3.14*RAIO_RODA*3.6); // Calculo da velocidade
+
+    Serial.print ("  VEL = ");
+    Serial.println (VEL);
+    pulsosVEL = 0;
+    Velocidade_millisInicial = millis();
+    
+    interrupts();
+  }
+}
+
+void tacometro() 
+{
+  pulsosVEL++; // Função chamada no pino de interrupção
+}
+
+void setup(){
+  Serial.begin(9600);
+  attachInterrupt (digitalPinToInterrupt(pinVEL), tacometro, RISING); //Interrupção para ler pulso da velocidade
+}
+void loop(){
+  velocidade();
+}
